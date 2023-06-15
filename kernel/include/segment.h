e4d71d1a4866325a4a650f86c591ad0259642901
Théo Rozier <theo.rozier@grenoble-inp.fr>
2023-06-15 13:02:42 +0200

    Commented segments and reencoded readmes

--- kernel/include/segment.h
+++ kernel/include/segment.h
@@ -3,11 +3,17 @@
 #ifndef __SEGMENT_H__
 #define __SEGMENT_H__
 
+/// Segment 1 in GDT (RPL = 0).
 #define BASE_TSS	0x08
-#define KERNEL_CS	0x10	/* Kernel's PL0 code segment */
-#define KERNEL_DS	0x18	/* Kernel's PL0 data segment */
-#define USER_CS		0x43	/* User's code descriptor, RPL=3 */
-#define USER_DS		0x4b	/* User's data descriptor, RPL=3 */
+/// Segment 2 in GDT (RPL = 0), code.
+#define KERNEL_CS	0x10
+/// Segment 3 in GDT (RPL = 0), data.
+#define KERNEL_DS	0x18
+/// Segment 8 in GDT (RPL = 3), code.
+#define USER_CS		0x43
+/// Segment 9 in GDT (RPL = 3), data.
+#define USER_DS		0x4b
+/// Segment 10+ in GDT (RPL = 0), task.
 #define TRAP_TSS_BASE	0x50
 
 #endif
