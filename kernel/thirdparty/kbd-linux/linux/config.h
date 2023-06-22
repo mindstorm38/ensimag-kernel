3a4b199d05333593323cc81092b42bdd1740c64c
Théo Rozier <theo.rozier@grenoble-inp.fr>
2023-06-22 14:24:50 +0200

    Removed thrid party keyboard because unused.

--- kernel/thirdparty/kbd-linux/linux/config.h
+++
@@ -1,6 +0,0 @@
-#ifndef _LINUX_CONFIG_H
-#define _LINUX_CONFIG_H
-
-#define CONFIG_SMP 1
-
-#endif
--- kernel/thirdparty/kbd-linux/linux/types.h
+++
@@ -1,8 +0,0 @@
-#ifndef _LINUX_TYPES_H
-#define _LINUX_TYPES_H
-
-typedef unsigned char u_char;
-typedef unsigned short u_short;
-typedef unsigned short ushort;
-
-#endif
